d16a05973435b1352dac5bb30de7be80c56fcbbe
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-20 01:38:11 +0900

    abc144のA,B問題を追加

---
+++ abc144/b/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var flag = false;
+            for(var i = 1; i <= 9; i++){
+                for (var j = 1; j <= 9; j++){
+                    if(i * j == n){
+                        flag = true;
+                        break;
+                    }                    
+                }
+            }
+
+            Console.WriteLine(flag ? "Yes" : "No");
+        }
+    }
+}
