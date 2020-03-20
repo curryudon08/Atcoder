7b5da31af897868acf3b32445c17afffbd56074d
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-21 01:38:24 +0900

    ABC153の問題Dの解答を追加

---
+++ abc153/d/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = BigInteger.Parse(Console.ReadLine());
+            Console.WriteLine(recFunc(h));
+        }
+
+        static BigInteger recFunc(BigInteger h){
+            if(h == 1){
+                return 1;
+            }
+            return recFunc(h / 2) * 2 + 1;
+        }
+    }
+}
