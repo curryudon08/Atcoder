7f0f59971c36460bff5c81a114ac932b41e9d738
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-21 01:38:01 +0900

    abc142の問題A,Bの解答を追加

---
+++ abc142/a/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = int.Parse(Console.ReadLine());
+            var odd = h % 2 == 0 ? h / 2 : h / 2 + 1;
+            Console.WriteLine(string.Format("{0:f10}",(1.0 * odd) / h));
+        }
+    }
+}
---
+++ abc142/b/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = Console.ReadLine().Split();
+            var n = int.Parse(_[0]);
+            var k = int.Parse(_[1]);
+            var h = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+
+            var count = h.Count(i => i >= k);
+            Console.WriteLine(count);
+        }
+    }
+}
