7664d1dc25deadca080d0ee5e1d394f0aa011a64
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-22 14:45:09 +0900

    abc140の問題A,Bの解答を追加

---
+++ abc140/a/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Math.Pow(n,3));
+        }
+    }
+}
---
+++ abc140/b/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+            var b = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+            var c = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+
+            var total = 0;
+            var pre = a[0] - 1;
+            foreach(var i in a){
+                var idx = i - 1;
+                total += b[idx];
+                if(pre + 1 == idx){
+                    total += c[idx - 1];
+                }
+                pre = idx;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
