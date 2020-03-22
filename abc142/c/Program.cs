2976e89a52791a544bbad58fda4780a265f3e87b
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-22 14:45:59 +0900

    ABC142の問題Cの解答を追加

---
+++ abc142/c/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var a = Console.ReadLine().Split().Select(i => int.Parse(i)).ToArray();
+
+            var d = new Dictionary<int, int>();
+            for(var i = 0; i < n; i++){
+                d.Add(i + 1, a[i]);
+            }
+
+            var sorted = d.OrderBy(k => k.Value);
+            Console.WriteLine(string.Join(" ",sorted.Select(v => v.Key.ToString())));
+        }
+    }
+}
