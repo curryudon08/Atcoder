bb062821634f28ca2398fbda1aeb9cfed2a28fe2
udonk <6565941+curryudon08@users.noreply.github.com>
2020-03-29 16:26:29 +0900

    コード追加

---
+++ abc149/d/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = Console.ReadLine().Split();
+            var n = int.Parse(_[0]);
+            var k = int.Parse(_[1]);
+
+            var __ = Console.ReadLine().Split();
+            var r = int.Parse(__[0]);
+            var s = int.Parse(__[1]);
+            var p = int.Parse(__[2]);
+
+            var t = Console.ReadLine().ToCharArray();
+            var h = new char[n];
+
+            var point = 0;
+            for(var i = 0; i < n; i++){
+                var idx = i - k;
+                if(t[i] == 'r'){
+                    if(idx >= 0 && h[idx] == 'p'){
+                        h[i] = 'x';
+                        point += 0;
+                    }else{
+                        h[i] = 'p';
+                        point += p;
+                    }
+                }else if(t[i] == 's'){
+                    if(idx >= 0 && h[idx] == 'r'){
+                        h[i] = 'x';
+                        point += 0;
+                    }else{
+                        h[i] = 'r';
+                        point += r;
+                    }
+                }else if(t[i] == 'p'){
+                    if(idx >= 0 && h[idx] == 's'){
+                        h[i] = 'x';
+                        point += 0;
+                    }else{
+                        h[i] = 's';
+                        point += s;
+                    }
+                }
+            }
+
+            Console.WriteLine(point);
+        }
+    }
+}
--- abc159/d/Program.cs
+++ abc159/d/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace d
 {
@@ -6,7 +8,31 @@ namespace d
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = long.Parse(Console.ReadLine());
+
+            var a = Console.ReadLine().Split().Select(i => long.Parse(i)).ToList();
+            var d = new Dictionary<long, long>();
+            foreach(var i in a){
+                if(d.ContainsKey(i)){
+                    d[i] += 1;
+                }else{
+                    d.Add(i, 1);
+                }
+            }
+
+            var total = 0L;
+            foreach(var i in d.Values){
+                total += (i * (i - 1)) / 2;
+            }
+
+            for(var i = 0; i < n; i++){
+                var val = d[a[i]];
+                var count = total;
+                count -= (val * (val - 1)) / 2;
+                count += ((val - 1) * (val - 2)) / 2;
+                Console.WriteLine(count);
+            }
+
         }
     }
 }
