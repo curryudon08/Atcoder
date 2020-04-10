3813a53dd7f94ae4200a0159b9a547b955536b6d
udonk <6565941+curryudon08@users.noreply.github.com>
2020-04-11 01:21:02 +0900

    解答を追加

---
+++ abc062/a/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var g1 = new int[]{1,3,5,7,8,10,12};
+            var g2 = new int[]{4,6,9,11};
+            var g3 = new int[]{2};
+            
+            var _ = Console.ReadLine().Split();
+            var x = int.Parse(_[0]);
+            var y = int.Parse(_[1]);
+
+            var flag = false;
+            if(g1.Contains(x) && g1.Contains(y)){
+                flag = true;
+            }
+            if(g2.Contains(x) && g2.Contains(y)){
+                flag = true;
+            }
+            if(g3.Contains(x) && g3.Contains(y)){
+                flag = true;
+            }
+
+            Console.WriteLine(flag ? "Yes" : "No");
+        }
+    }
+}
---
+++ abc062/b/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = Console.ReadLine().Split();
+            var h = int.Parse(_[0]);
+            var w = int.Parse(_[1]);
+
+            var s = new string[h];
+            for(var i = 0; i < h; i++){
+                s[i] = Console.ReadLine().Trim();
+            }
+
+            Console.WriteLine(new string('#', w + 2));
+            for(var i = 0; i < h; i++){
+                Console.WriteLine("#" + s[i] + "#");
+            }
+            Console.WriteLine(new string('#', w + 2));
+        }
+    }
+}
